7b26f0e6b1541deb7e5635ca4d6eedc03cbc021d
rpdentrechales <rpdprocorpo@gmail.com>
2025-01-15 18:42:24 +0000

    Updating files

--- views/configurar.py
+++ views/configurar.py
@@ -35,8 +35,7 @@ if seletor_pagina == "Prestadoras":
   if st.button("Salvar alterações"):
 
     st.session_state["dados_prestadoras"] = edited_prestadora_df
-    st.write(edited_prestadora_df.columns)
-    result = sync_dataframe(collection_name="prestadores_db",database_name="relatorio_prestadoras", dataframe=edited_prestadora_df, unique_key="nome_prestador")
+    result = sync_dataframe(collection_name="prestadores_db",database_name="relatorio_comissao", dataframe=edited_prestadora_df, unique_key="nome_prestador")
     st.success("Alterações salvas com sucesso!")
 
 if seletor_pagina == "Comissões":
