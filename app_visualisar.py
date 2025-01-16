ce8538b9ac80bbbce05a8a4e6628d67848e1e8b0
rpdentrechales <rpdprocorpo@gmail.com>
2025-01-16 14:40:15 +0000

    Updating files

---
+++ app_visualisar.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+# --- PAGE SETUP ---
+visualizar_page = st.Page(
+    "views/visualisar.py",
+    title="Visualisar",
+    icon=":material/payments:"
+)
+# configurar_page = st.Page(
+#     "views/configurar_vendedoras.py",
+#     title="Configurar Vendedoras",
+#     icon=":material/manufacturing:",
+#     default=True
+# )
+
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Visualisar": [visualizar_page],
+        # "Testes": [configurar_page]
+    }
+)
+
+
+# --- SHARED ON ALL PAGES ---
+# st.logo("assets/codingisfun_logo.png")
+
+
+# --- RUN NAVIGATION ---
+pg.run()
