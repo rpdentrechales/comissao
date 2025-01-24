cc8575fc29b88ab95233a9b6fa20a4b8a500d043
rpdentrechales <115180489+rpdentrechales@users.noreply.github.com>
2025-01-24 11:19:26 -0300

    bugfix

--- views/configurar.py
+++ views/configurar.py
@@ -86,7 +86,7 @@ if seletor_pagina == "Comissões":
   }
 
   comissao_df = comissao_df[column_order_comissao]
-  comissao_df = comissao_df.sort_values(by=["Procedimento"], ascending=[True],na_position="first")
+  comissao_df = comissao_df.sort_values(by=["Valor","Procedimento"], ascending=[True,True],na_position="first")
 
   edited_comissao_df = st.data_editor(comissao_df,
                                       use_container_width=False,
