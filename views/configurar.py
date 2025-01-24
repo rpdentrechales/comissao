6545c89b89ec93a43c8b8d34d21e5e3231812b09
rpdentrechales <115180489+rpdentrechales@users.noreply.github.com>
2025-01-24 11:55:06 -0300

    teste

--- views/configurar.py
+++ views/configurar.py
@@ -88,7 +88,7 @@ if seletor_pagina == "Comissões":
   comissao_df = comissao_df[column_order_comissao]
   comissao_df = comissao_df.sort_values(by=["Valor","Procedimento"], ascending=[True,True],na_position="first")
   comissao_df["Valor"] = comissao_df["Valor"].fillna("")
-  
+  st.write("Chegou aqui")
   edited_comissao_df = st.data_editor(comissao_df,
                                       use_container_width=False,
                                       hide_index=True,
