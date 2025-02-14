229f8b4edbdd4240d97535ce924475785f2c0ac3
rpdentrechales <rpdprocorpo@gmail.com>
2025-02-14 12:21:32 -0300

    bugfix

--- views/comissoes.py
+++ views/comissoes.py
@@ -40,8 +40,7 @@ if processar_button:
 
     periodos = base_compilada["periodo"].unique()
     periodos_series = pd.Series(periodos)
-    periodos_list = "periodo: " + periodos_series.dt.strftime('%Y-%m-%d')
-    periodos_list = periodos.to_list()
+    periodos_list = "periodo: " + periodos_series.dt.strftime('%Y-%m-%d').to_list()
     periodos = periodos.insert(0,"mensal")
 
     seletor_periodo = st.pills("Selecione Visão",periodos,selection_mode="single",default="mensal")
